a8160febd31dd9eb5506a16c0e6c3d09e0207a12
GordonOrr <103220757+GordonOrr@users.noreply.github.com>
2022-07-03 15:11:57 -0400

    Update game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,18 @@
+// This creates a variable to hold answer and 
+// Prompt for input to question
+var playerName = window.prompt("That is your robot's name?");
+// window.alert(playerName);
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// This will do math and log 20
+console.log(10+10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+console.log("Your robot, " + playerName + ", has won!");
+
 // this creates a function named "fight"
 function fight() {
     window.alert("The fight has begun!");
 }
+// This executes "fight" function
+// fight();
